db9a0c2570459db710cd591e3c8f75f9a6fb6cc2
Chris Henson <chrishenson.net@gmail.com>
2024-04-02 13:02:46 -0400

    llvm tutorial assignment

--- tutorial/first_llvm_project/hello.cc
+++ tutorial/first_llvm_project/hello.cc
@@ -57,16 +57,15 @@ static cl::opt<std::string> FileName(cl::Positional,
                                      cl::Required);
 
 // TODO - uncomment for your assignment
-// void printInstructionList(Function &F) {
-//     for (/**/) { // Fill in this section to iterate through each basic block in the function
-//         for (/**/) { // Fill in this section to iterate through each instruction in the basic block
-               // Do something with each instruction I, e.g., print it:
-//             I->print(errs());
-//             errs() << "\n";
-//         }
-//     }
-// }
-
+void printInstructionList(Function &F) {
+    for (Function::iterator BB = F.begin(); BB != F.end(); ++BB) { // Fill in this section to iterate through each basic block in the function
+        for (BasicBlock::iterator I = BB->begin(); I != BB->end(); ++I) { // Fill in this section to iterate through each instruction in the basic block
+            // Do something with each instruction I, e.g., print it:
+            I->print(errs());
+            errs() << "\n";
+        }
+    }
+}
 
 /*
  * 1. 'argc':
@@ -199,9 +198,9 @@ int main(int argc, char** argv)
             outs() << "Function name - " << i->getName() << "\n";
 
             // TODO - uncomment for your assignment
-            // outs() << "Instructions - " << "\n";
-            // printInstructionList(const_cast<Function&>(*i));
-            // outs() << "\n";
+            outs() << "Instructions - " << "\n";
+            printInstructionList(const_cast<Function&>(*i));
+            outs() << "\n";
         }
     }
 
